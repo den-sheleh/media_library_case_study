56ef31b6e6422c4e6aa1fe09a39dc0b293e79c78
Dennis Sheleh <dsheleh@rocketfuel.com>
2015-02-08 14:33:40 +0300

    MediaItem: create and index

--- app/controllers/media_items_controller.rb
+++ app/controllers/media_items_controller.rb
@@ -1,4 +1,25 @@
 class MediaItemsController < ApplicationController
   def index
+    @media_items = current_user.media_items
+  end
+
+  def new
+    @media_item = MediaItem.new
+  end
+
+  def create
+    @media_item = current_user.media_items.build(media_item_params)
+
+    if @media_item.save
+      redirect_to(media_item_images_path(@media_item), notice: t('media_item.success_add'))
+    else
+     render action: 'new'
+    end
+  end
+
+  private
+
+  def media_item_params
+    params.require(:media_item).permit(:title)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
 
   root to: 'media_items#index'
+
+  resources :media_items, only: [:index, :new, :create] do
+    resources :images
+  end
 end
